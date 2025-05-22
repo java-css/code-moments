401462174e4f9a366efbe1af53f6a2892991f6f0
Hello Jason <1053384819@qq.com>
2025-05-23 01:19:39 +0800

    添加base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/code-moments/",
   title: "前端拾光",
   description: "个人前端知识博客",
   themeConfig: {
